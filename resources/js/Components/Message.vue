ca53b3a32075aefa3d5fa8a5c4faa326b82c002f
Won Hee Qin <fourtwentyblaise@gmail.com>
2023-07-17 21:23:18 +0800

    completed laptop version, small edits overall

--- resources/js/Components/Message.vue
+++ resources/js/Components/Message.vue
@@ -14,22 +14,22 @@ let copyEmail = (e) => {
 <template>
     <div class="flex flex-row justify-between bg-gray-200 p-2">
         <div class="flex flex-col">
-            <div class="flex flex-row">
-                <p class="text-lg font-bold">{{ props.source.title }}</p>
-                <p class="text-lg ml-2">by {{ props.source.titleUserName }}</p>
+            <div class="flex flex-row items-center">
+                <p class="text-sm 1440:text-lg font-bold">{{ props.source.title }}</p>
+                <p class="text-sm 1440:text-lg ml-2">by {{ props.source.titleUserName }}</p>
                 <img
-                    class="ml-1.5"
+                    class="ml-1.5 email"
                     src="images/mail.svg"
                     v-b-tooltip.hover title="Copy Email Address to Clipboard"
                     @click="copyEmail(props.source.titleUserId)"
                     />
             </div>
-            <p v-for="p in props.source.content">
+            <p v-for="p in props.source.content" class="laptop:text-xs">
                 {{ p }}
             </p>
-            <p class="text-sm">Message created at {{ props.source.timestamp }}</p>
+            <p class="text-xs 1440:text-sm">Message created at {{ props.source.timestamp }}</p>
         </div>
-        <div v-show="props.source.title=='Substitution Request'" class="flex flex-row justify-evenly pl-2 w-36 border-l-4 border-white">
+        <div v-show="props.source.title=='Substitution Request'" class="flex flex-row justify-evenly pl-2 w-36 1440:w-48 border-l-4 border-white">
             <div class="flex flex-col justify-center">
                 <button class="flex flex-col items-center">
                     <img src="images/accept.svg"/>
@@ -43,7 +43,7 @@ let copyEmail = (e) => {
                 </button>
             </div>
         </div>
-        <div v-show="props.source.title!='Substitution Request'" class="flex flex-row justify-evenly space-x-2 px-4 w-36 border-l-4 border-white">
+        <div v-show="props.source.title!='Substitution Request'" class="flex flex-row justify-evenly space-x-2 px-4 w-36 1440:w-48 border-l-4 border-white">
             <div class="flex flex-col justify-center ">
                 <button class="flex flex-col items-center">
                     <img src="images/acknowledge.svg"/>
@@ -53,3 +53,19 @@ let copyEmail = (e) => {
         </div>
     </div>
 </template>
+
+<style>
+.email{
+    height: 18px;
+    width: 18px;
+}
+
+/* 1440p */
+@media 
+(min-width: 1930px) {
+.email{
+    height: 24px;
+    width: 24px;
+}
+}
+</style>
--- resources/js/Components/Shortcut.vue
+++ resources/js/Components/Shortcut.vue
@@ -4,11 +4,11 @@ import { Link } from '@inertiajs/vue3';
 let props = defineProps({ stripBG: String, href: String });
 </script>
 <template>
-    <Link :href="props.href" class="flex flex-col justfiy-between rounded-3xl mx-4 text-2xl text-center">
-        <div class="font-bold text-2xl p-4 justify-center mt-4" >
+    <Link :href="props.href" class="flex flex-col justfiy-between rounded-3xl 1440:mx-4 text-2xl text-center mx-2">
+        <div class="font-bold laptop:text-sm 1440:text-2xl 1440:p-4 justify-center mt-4 mx-6" >
             <slot />
         </div>
-        <div class="flex flex-col pt-4 px-4 items-center">
+        <div class="flex flex-col 1440:pt-4 my-auto py-2 items-center">
             <slot name="content" />
         </div>
         <slot name="strip" />
