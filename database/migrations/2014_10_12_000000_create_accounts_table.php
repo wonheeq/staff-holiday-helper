56654ed8fcf855ec9b7040df4100f1c7c265f306
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-04 16:27:29 +0800

    Change users table to accounts, attributes edited

---
+++ database/migrations/2014_10_12_000000_create_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->char('accountNo', 7)->primary();
+            $table->string('accountType', 21);
+            $table->string('fName', 30);
+            $table->string('lName', 20)->nullable();
+            // No size limit on password for now, unsure about encryption
+            $table->string('password');
+            $table->char('superiorNo')->nullable();
+            // Left out schoolId for now, not necessary for home page or bookings page
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::table('accounts',function (Blueprint $table) {
+            $table->foreign('superiorNo')->references('accountNo')->on('accounts')->cascadeOnUpdate()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
