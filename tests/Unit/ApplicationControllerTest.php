61e72ab9fad1afa6cae56e8de847108a6d1f3d3d
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-04 20:17:12 +0800

    Trying to get factories to return all attributes

--- tests/Unit/ApplicationControllerTest.php
+++ tests/Unit/ApplicationControllerTest.php
@@ -4,55 +4,57 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
-use App\Models\User;
+use App\Models\Account;
 use App\Models\Application;
+use Illuminate\Support\Facades\Log;
 
 class ApplicationControllerTest extends TestCase
 {
-    private User $user;
+    private Account $user;
     private array $applications;
 
     public function setup(): void {
         parent::setup();
 
-        $this->user = User::factory()->create();
+        $this->user = Account::factory()->create();
         $this->applications = array();
 
 
         for($i = 0; $i < 5; $i++) {
             array_push($this->applications, Application::factory()->create([
-                'accountNo' => $this->user->id
+                'accountNo' => $this->user->accountNo
             ]));
         }
+
+        Log::debug($this->applications);
     }
 
     public function teardown(): void {
         parent::teardown();
         foreach ($this->applications as $app) {
-            $app->delete();
+            Application::where('applicationNo', $app['applicationNo'])->delete();
         }
-        $this->user->delete();
+        Account::where('accountNo', $this->user['accountNo'])->delete();
     }
 
     public function test_api_request_for_applications_successful(): void
     {
         // Check for valid response
-        $response = $this->getJson("/api/applications/{$this->user->id}");
+        $response = $this->getJson("/api/applications/{$this->user->accountNo}");
         $response->assertStatus(200);
     }
 
     public function test_api_request_for_applications_invalid_user(): void
     {
         // Check for invalid response
-        $response = $this->getJson('/api/applications/a1234545354356');
+        $response = $this->getJson('/api/applications/asfasfasfasf');
         $response->assertStatus(500);
     }
 
     public function test_api_request_for_applications_valid_length(): void
     {
-      
-        // Test if amount of applications is 5
-        $response = $this->getJson("/api/applications/{$this->user->id}");
+        // Test if amount of applications matches
+        $response = $this->getJson("/api/applications/{$this->user->accountNo}");
         $array = $response->getData();
         $this->assertTrue(count($array) == count($this->applications));
     }
@@ -60,20 +62,20 @@ class ApplicationControllerTest extends TestCase
     public function test_api_request_for_applications_content_is_json(): void
     {
         // Check if response is json
-        $response = $this->getJson("/api/applications/{$this->user->id}");
+        $response = $this->getJson("/api/applications/{$this->user->accountNo}");
         $this->assertJson($response->content());
     }
 
     public function test_api_request_for_applications_content_is_valid(): void
     {
         // Check if correct structure
-        $response = $this->get("/api/applications/{$this->user->id}");
+        $response = $this->get("/api/applications/{$this->user->accountNo}");
         $response->assertJsonStructure([
             0 => [
-                'id',
+                'applicationNo',
                 'accountNo',
-                'start',
-                'end',
+                'sDate',
+                'eDate',
                 'status',
                 'processedBy',
                 'rejectReason',
@@ -82,8 +84,8 @@ class ApplicationControllerTest extends TestCase
 
         // Check if returned applications for correct user
         $array = json_decode($response->content());
-        foreach ($array as $message) {
-            $this->assertTrue($message->accountNo == $this->user->id);
+        foreach ($array as $app) {
+            $this->assertTrue($app->accountNo == $this->user->accountNo);
         }
     }
 }
