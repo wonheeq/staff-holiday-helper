2cef0eb82d92003f001cd15af70baf26b8e8b98c
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-06 14:55:42 +0800

    Move getRoleFromAcountRoleId to RoleController

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+     /*
+    Returns the formatted role name of the given AccountRoleId
+    */
+    public function getRoleFromAccountRoleId($accountRoleId) {
+        $accountRole = AccountRole::where('accountRoleId', $accountRoleId)->first();
+        $role = Role::where('roleId', $accountRole['roleId'])->first();
+        $roleName = $role['name'];
+
+        // TODO: use unitId, majorId, courseId to determin real name of task after they are implemented
+
+        $task = "UNITCODE Unit Name - {$roleName}";
+        return $task;
+    }
 }
