1f34cf310ef2d548793bdb1692d0f025013d93fb
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-06 16:00:33 +0800

    Auth Update: Narrowed error down to password access. Only fails when accountNo is real, and it tries to check pswd

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,6 +26,7 @@ class LoginController extends Controller
             $request->session()->regenerate();
             return redirect()->intended('home');
         }
+        error_log('after attempt');
 
         return back()->withErrors([
             'accountNo' => 'Provided credentials do not match',
