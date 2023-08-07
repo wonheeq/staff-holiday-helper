8d394c5e510146af4d328502b1b31de755e898b6
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-07 15:03:44 +0800

    basic test layout

--- resources/js/Components/Bookings/SubstitutionsSubpage.vue
+++ resources/js/Components/Bookings/SubstitutionsSubpage.vue
@@ -1,5 +1,50 @@
+<script setup>
+import VueScrollingTable from "vue-scrolling-table";
+import "/node_modules/vue-scrolling-table/dist/style.css";
+let deadAreaColor = "#FFFFFF";
+
+let substitutions = [
+    {
+        task: "test",
+        sDate: "2023-08-03 14:30",
+        eDate: "2023-08-04 14:30",
+        applicantName: "Test"
+    },
+    {
+        task: "test",
+        sDate: "2023-08-03 14:30",
+        eDate: "2023-08-04 14:30",
+        applicantName: "Test"
+    },
+    {
+        task: "test",
+        sDate: "2023-08-03 14:30",
+        eDate: "2023-08-04 14:30",
+        applicantName: "Test"
+    },
+];
+
+</script>
 <template>
     <div>
-        Substitutions subpage
+        <p>
+            Substitutions subpage
+        </p>
+
+        <VueScrollingTable
+            :deadAreaColor="deadAreaColor"
+            :scrollHorizontal="false"
+        >
+            <template #tbody>
+                <div v-for="item in substitutions" :key="item.id" class="mb-2">
+                    <p>
+                        {{ item.task }} for {{  item.applicantName }}
+                    </p>
+                    <p>
+                        Duration: {{ item.sDate }} - {{ item.eDate }}
+                    </p>
+                </div>
+            </template>
+        </VueScrollingTable>
     </div>
 </template>
