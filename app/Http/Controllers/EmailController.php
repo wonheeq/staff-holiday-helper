495c4aefb230df8a78063d2ce129cb50d0b631b8
Benjamin <20456890@student.curtin.edu.au>
2023-08-08 16:54:07 +0800

    Added applicationAccepted email sending method

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -10,7 +10,21 @@ class EmailController extends Controller
 
     public function sendEmail()
     {
-        $this->bookingEditTest();
+        //$this->bookingEditTest();
+        $this->applicationAccept();
+    }
+    public function applicationAccept()
+    {
+        $dynamicData = [
+            'name' => 'Benny',
+            'appNo' => 123123,
+            'nName' => 'Messi',
+            'role' => 'Lecturer',
+            'uCode' => 'COMP3003',
+            'uName' => "Foundation of Computer Science and Data Engineering",
+            'period' => '00:00 23/04/2022 - 00:00 25/04/2022'
+        ];
+        Mail::to("tvxqbenjamin0123@gmail.com")->send(new MJML("Application Accepted", "email/applicationAccepted", $dynamicData));
     }
     public function bookingEditTest()
     {
