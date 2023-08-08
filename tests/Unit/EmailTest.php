198d8fa77e6e086dd65ea73609305c607bf2bc2c
Benjamin <20456890@student.curtin.edu.au>
2023-08-08 16:54:41 +0800

    Added unit tests for applicationAccepted email

--- tests/Unit/EmailTest.php
+++ tests/Unit/EmailTest.php
@@ -12,6 +12,33 @@ use Illuminate\Support\Testing\Concerns\InteractsWithPages;
 class EmailTest extends TestCase
 {
 
+    public function testApplicationApproval(): void
+    {
+        $dynamicData = [
+            'name' => 'Benny',
+            'appNo' => 123123,
+            'nName' => 'Messi',
+            'role' => 'Lecturer',
+            'uCode' => 'COMP3003',
+            'uName' => "Foundation of Computer Science and Data Engineering",
+            'period' => '00:00 23/04/2022 - 00:00 25/04/2022'
+        ];
+        $mailable = new MJML("Application Accepted", "email/applicationAccepted", $dynamicData);
+        Mail::fake(); //a method that sends the email without actually sending it
+        Mail::to("tvxqbenjamin0123@gmail.com")->send($mailable);
+        Mail::assertSent(MJML::class); //check if email has really been sent
+        $this->assertEquals('tvxqbenjamin0123@gmail.com', $mailable->to[0]['address']); //check if receiver's email is correct
+        $this->assertEquals('Application Accepted', $mailable->subject); //check if subject of the email is correct
+        
+        //after render, check if the rendered content contains below strings/details:
+        $this->assertStringContainsString('Benny', $mailable->render());
+        $this->assertStringContainsString('123123', $mailable->render());
+        $this->assertStringContainsString('Messi', $mailable->render());
+        $this->assertStringContainsString('Lecturer', $mailable->render());
+        $this->assertStringContainsString('COMP3003', $mailable->render());
+        $this->assertStringContainsString('Foundation of Computer Science and Data Engineering', $mailable->render());
+        $this->assertStringContainsString('00:00 23/04/2022 - 00:00 25/04/2022', $mailable->render());
+    }
     public function testEmailContents(): void
     {
         $nominees = new Nominees();
