ec01280727b5afb60e1fc3df8ddc0755f74ba39f
Aden Moore <20562959@student.curtin.edu.au>
2023-08-09 04:05:08 +0800

    features/basic_api_tests_for_5_tables/1

---
+++ tests/Unit/AccountsControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+
+class AccountsControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_api_request_for_all_accounts(): void
+    {
+        $response = $this->getJson('/api/accounts');
+        $response->assertStatus(200);
+    }
+
+    public function test_api_request_for_accounts_content_is_json(): void
+    {
+        // Check if response is json
+        $response = $this->getJson("/api/accounts/");
+        $this->assertJson($response->content());
+    }
+
+    public function test_api_request_for_accounts_content_is_valid(): void
+    {
+        // Check if correct structure
+        $response = $this->get("/api/accounts/");
+        $response->assertJsonStructure([
+            0 => [
+                'accountNo',
+                'accountType',
+                'lName',
+                'fName',
+                'password',
+                'superiorNo',
+                'schoolId'
+            ],
+        ]);
+    }
+}
---
+++ tests/Unit/MinorTableControllerTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class MinorTableControllerTest extends TestCase
+{
+    /**
+     * Tests Units, Majors, Courses and Schools
+     */
+    public function test_units_api_requests(): void
+    {
+        $response = $this->getJson('/api/units');
+        $response->assertStatus(200);
+
+         // Check if response is json
+         $response = $this->getJson("/api/units/");
+         $this->assertJson($response->content());
+
+         // Check if correct structure
+        $response = $this->get("/api/units/");
+        $response->assertJsonStructure([
+            0 => [
+                'unitId',
+                'name'
+            ],
+        ]);
+    }
+
+    public function test_majors_api_requests(): void
+    {
+        $response = $this->getJson('/api/majors');
+        $response->assertStatus(200);
+
+         // Check if response is json
+         $response = $this->getJson("/api/majors/");
+         $this->assertJson($response->content());
+
+         // Check if correct structure
+        $response = $this->get("/api/majors/");
+        $response->assertJsonStructure([
+            0 => [
+                'majorId',
+                'name'
+            ],
+        ]);
+    }
+
+    public function test_courses_api_requests(): void
+    {
+        $response = $this->getJson('/api/courses');
+        $response->assertStatus(200);
+
+         // Check if response is json
+         $response = $this->getJson("/api/courses/");
+         $this->assertJson($response->content());
+
+         // Check if correct structure
+        $response = $this->get("/api/courses/");
+        $response->assertJsonStructure([
+            0 => [
+                'courseId',
+                'name'
+            ],
+        ]);
+    }
+
+    public function test_schools_api_requests(): void
+    {
+        $response = $this->getJson('/api/schools');
+        $response->assertStatus(200);
+
+         // Check if response is json
+         $response = $this->getJson("/api/schools/");
+         $this->assertJson($response->content());
+
+         // Check if correct structure
+        $response = $this->get("/api/schools/");
+        $response->assertJsonStructure([
+            0 => [
+                'schoolId',
+                'name'
+            ],
+        ]);
+    }
+}
