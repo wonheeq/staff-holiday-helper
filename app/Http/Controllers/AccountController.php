0275d6ca272c80fe481a4503021bf51acd304c2a
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-16 15:51:33 +0800

    Implemented getCurrentLineManager Implemented getDefaultAdmin user account if superior doesn't exist

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Account;
 
 use Illuminate\Http\Request;
+define("DEFAULT_ADMIN_ACCOUNT_NO", "000002L");
 
 class AccountController extends Controller
 {
@@ -17,5 +18,26 @@ class AccountController extends Controller
         //return response()->json($accounts);
 
         return Account::all(); 
+    }
+
+    public function getDefaultAdmin() {
+        return Account::where('accountNo', DEFAULT_ADMIN_ACCOUNT_NO)->first();
+    }
+
+    public function getCurrentLineManager(String $accountNo) {
+        try {
+            // Attempt to get user
+            $user = Account::where('accountNo', $accountNo)->first();
+            $superiorNo = $user->superiorNo;
+
+            if ($superiorNo == null) {
+                throw new ErrorException();
+            }
+            return Account::where('accountNo', $superiorNo)->first();
+        }
+        catch (Exception $e) {
+            // Error occurred - return default admin user
+            return $this->getDefaultAdmin();
+        }
     }
 }
