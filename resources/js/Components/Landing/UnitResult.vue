c8803cd58e7fb3233031c22cc1f34b5314a46032
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-18 16:59:00 +0800

    Results Display Tweaks

--- resources/js/Components/Landing/UnitResult.vue
+++ resources/js/Components/Landing/UnitResult.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 
         <!-- Results Display -->
         <h1 class="font-bold text-2xl mb-1">Showing Result For:</h1>
-        <h2 class="font-bold mb-5">{{ unitName }} ({{ unitId }})</h2>
+        <h2 class="font-bold mb-4">{{ unitName }} ({{ unitId }})</h2>
         <p class="mb-1">Currently Responsible: {{ name }}</p>
         <p class="mb-7">Staff Email: {{ email }}</p>
 
