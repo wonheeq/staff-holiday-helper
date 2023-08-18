c1d3e3ecc047edc93dc32565967b07900eec3133
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-18 17:15:09 +0800

    Base Harness Made - Broken

---
+++ tests/Unit/UnitLookupTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Unit;
+use App\Models\AccountRole;
+use App\Models\Account;
+use App\Models\Major;
+use App\Models\Course;
+use App\Models\School;
+
+class UnitLookupTest extends TestCase
+{
+
+    protected function setup(): void
+    {
+        Unit::create([
+            'unitId' => 'AAAA0000',
+            'name' => 'tempName',
+        ]);
+
+        AccountRole::create([
+            'accountNo' => '000000a',
+            'roleId' => 1,
+            'unitId' => 'AAAA0000',
+            'majorId' => fake()->randomElement(Major::pluck('majorId')),
+            'courseId' => fake()->randomElement(Course::pluck('courseId')),
+            'schoolId' => fake()->randomElement(School::pluck('schoolId')),
+        ]);
+        parent::setup();
+    }
+
+    protected function tearDown(): void
+    {
+        AccountRole::where([
+            ['accountNo', '=', '000000a'],
+            ['roleId', '=', 1],
+            ['unitId', '=', 'AAAA0000']
+        ])->delete();
+
+        Unit::where('unitId', 'AAAA0000')->delete();
+        parent::tearDown();
+    }
+
+    public function test_lookup_valid_unit(): void
+    {
+        $response = $this->post('/api/getUnitDetails', [
+            'code' => 'AAAA0000'
+        ])->assertStatus(200);
+    }
+}
