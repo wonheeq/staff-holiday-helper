a7f0926147967a4134ba4d00f7a2410370bc4db6
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-20 15:24:10 +0800

    Actually fixed 1440 scaling

--- resources/js/Components/Landing/UnitResult.vue
+++ resources/js/Components/Landing/UnitResult.vue
@@ -20,6 +20,7 @@ function getList()
         str += 'Name: ' + name + '\n';
         str += 'Email: ' + email + '\n\n';
 
+
     });
     str = str.slice(0, -6);
     console.log(str);
@@ -33,7 +34,8 @@ function getList()
     <!-- Box/Background -->
     <!-- <div class=" laptop:w-[25%] 1080:w-[20%] 1440:w-[17%] 4k:w-[14%] h-fit bg-white p-5 drop-shadow-md"> -->
     <div class=" laptop:w-[25%] 1080:w-[20%] 1440:w-[17%] 4k:w-[14%] bg-white p-5 drop-shadow-md
-                 laptop:h-[80%] 1080:h-[80%] 1440:h-[60%] 4k:h-[60%]" >
+                 laptop:h-fit 1080:h-fit 1440:h-fit 4k:h-fit
+                 laptop:max-h-[80%] 1080:max-h-[80%] 1440:max-h-[60%] 4k:max-h-[60%]" >
 
         <!-- Logo -->
         <!-- <img src="/images/logo-horizontal.svg" alt="Logo Horizontal" class="mx-auto mb-5" > -->
@@ -43,7 +45,9 @@ function getList()
         <h2 class="font-bold mb-1 4k:text-2xl">{{ results.data.unitName }} ({{ results.data.unitId }})</h2>
         <h2 class="font-bold mb-1 4k:text-2xl">Currently Responsible Staff:</h2>
 
-        <div class="mb-7 overflow-y-auto laptop:h-[65%] 1080:h-[77%] 1440:h-[78%]">
+        <!-- <div class="mb-7 overflow-y-auto laptop:h-[65%] 1080:h-[77%] 1440:h-[78%] 4k:h-fit 4k:max-h-[20%]"> -->
+        <div class="mb-7 overflow-auto 1440:h-fit 1440:max-h-[37rem]">
+
             <h2 class="mt-5 mb-1 font-bold 4k:text-xl">Course Coordinator:</h2>
             <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.courseCoord[1] }}</h1>
             <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.courseCoord[0] }}</h1>
