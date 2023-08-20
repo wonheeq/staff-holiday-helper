cdda9de543fbb1b81f8e8e09014c945bd366a185
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-20 20:18:00 +0800

    Fixed Vertical scaling issue

--- resources/js/Components/Landing/UnitResult.vue
+++ resources/js/Components/Landing/UnitResult.vue
@@ -27,7 +27,8 @@ function getList()
     <!-- Box/Background -->
     <div class=" laptop:w-[25%] 1080:w-[20%] 1440:w-[17%] 4k:w-[14%] bg-white p-5 drop-shadow-md
                  laptop:h-fit 1080:h-fit 1440:h-fit 4k:h-fit
-                 laptop:max-h-[80%] 1080:max-h-[80%] 1440:max-h-[60%] 4k:max-h-[60%]" >
+                 laptop:max-h-[80%] 1080:max-h-[80%] 1440:max-h-[60%] 4k:max-h-[60%]
+                 flex flex-col" >
 
         <!-- Results Heading -->
         <h1 class="font-bold text-2xl 4k:text-3xl mb-1">Showing Results For:</h1>
@@ -35,7 +36,9 @@ function getList()
         <h2 class="font-bold mb-1 4k:text-2xl">Currently Responsible Staff:</h2>
 
         <!-- Results Display -->
-        <div class="mb-7 overflow-auto h-fit 1440:max-h-[37rem] 1080:max-h-[39rem] laptop:max-h-[23rem] 4k:max-h-[50rem]">
+        <!-- <div class="mb-7 overflow-auto h-fit 1440:max-h-[37rem] 1080:max-h-[36rem] laptop:max-h-[23rem] 4k:max-h-[50rem]"> -->
+        <div class="mb-7 overflow-auto  1440:max-h-[37rem] 1080:max-h-[32rem] laptop:max-h-[23rem] 4k:max-h-[50rem]">
+
 
             <h2 class="mt-5 mb-1 font-bold 4k:text-xl">Course Coordinator:</h2>
             <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.courseCoord[1] }}</h1>
