1b8a63fe92d05fdf07207edc9e47b5b751e8c815
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-25 16:16:49 +0800

    Authentication Testing Changes:  - moved all tests into AuthenticationControllerTest.php  - general cleanup and commenting

--- tests/Unit/AuthenticationControllerTest.php
+++ tests/Unit/AuthenticationControllerTest.php
@@ -14,28 +14,32 @@ class AuthenticationControllerTest extends TestCase
     {
         parent::setup();
 
-
         // insert  temporary account into Database for testing
         DB::table('accounts')->insert([
             'accountNo' => 'AAAAAA1',
             'accountType' => fake()->randomElement(['staff', 'lmanager', 'sysadmin']),
             'lName' => fake()->lastName(),
             'fName' => fake()->firstName(),
-            'password' => Hash::make('knownPassword7'),
+            'password' => Hash::make('knownPassword1'),
             'superiorNo' => fake()->randomElement(['112237t', '123456a', '441817e', '877873p']),
             'schoolId' => '101',
         ]);
     }
 
+
+
     protected function teardown(): void
     {
         // Delete temporary account from database
         DB::table('accounts')->where('accountNo', '=', 'AAAAAA1')->delete();
+        DB::table('accounts')->where('accountNo', '=', 'AAAAAA2')->delete();
 
         parent::teardown();
     }
 
-    // Test for login that has credentials, but are invalid
+
+
+    // Test for correct behaviour when loggin in with no credentials
     public function test_login_invalid_credentials(): void
     {
         // create request with credentials, test for correct 302 response
@@ -50,7 +54,8 @@ class AuthenticationControllerTest extends TestCase
     }
 
 
-    // test login with no credentials entered
+
+    // Test for correct behaviour when logging in with no credentials
     public function test_login_no_credentials(): void
     {
         // create empty request, test for correct 302 response
@@ -61,12 +66,15 @@ class AuthenticationControllerTest extends TestCase
         ])->assertStatus(302);
     }
 
+
+
+    // Test for correct behaviour when logging in with valid credentials
     public function test_login_valid_credentials(): void
     {
         // test for correct 302 response
         $response = $this->post('/login', [
             'accountNo' => 'AAAAAA1',
-            'password' => 'knownPassword7'
+            'password' => 'knownPassword1'
         ])->assertStatus(302);
 
         // Test that content returned contains 'Redirecting'
@@ -76,6 +84,9 @@ class AuthenticationControllerTest extends TestCase
         $this->assertTrue(str_contains($response->content(), "/home"));
     }
 
+
+
+    // Test for correct behaviour when logging out
     public function test_logout(): void
     {
         $response = $this->post('/logout')->assertStatus(200);
@@ -88,5 +99,140 @@ class AuthenticationControllerTest extends TestCase
             'response' => 'success',
             'url' => 'http://localhost',
         ]);
+    }
+
+
+
+    // Test the sending of a password reset email using valid credentials
+    public function test_sending_reset_email_valid_credentials(): void
+    {
+        $response = $this->post('/reset-password', [
+            'email' => 'AAAAAA1@curtin.edu.au',
+            'accountNo' => 'AAAAAA1'
+        ])->assertStatus(200);
+
+        $this->assertJson($response->content());
+
+        $response->assertJson([
+            'status' => 'We have emailed your password reset link.',
+        ]);
+    }
+
+
+
+    // Test for correct behaviour when request a password reset email with no credentials provided
+    public function test_sending_reset_email_no_credentials(): void
+    {
+        $response = $this->post('/reset-password', [
+            'email' => 'AAAAAB1@curtin.edu.au',
+            'accountNo' => 'AAAAAB1'
+        ])->assertStatus(302);
+
+        // Except the validation exception from no email
+        $this->expectException(ValidationException::class);
+        $response->assertJson([
+            'message' => 'The email field must be a valid email address.',
+        ]);
+    }
+
+
+
+    // Test for correct behaviour when request a password reset email with invalid credentials provided
+    public function test_sending_reset_email_bad_credentials(): void
+    {
+        $response = $this->post('/reset-password', [
+            'email' => 'AAAAAB1',
+            'accountNo' => 'AAAAAB1'
+        ])->assertStatus(302);
+
+        // Except the validation exception from no email
+        $this->expectException(ValidationException::class);
+        $response->assertJson([
+            'message' => 'The email field must be a valid email address.',
+        ]);
+    }
+
+
+
+    // Test for correct behaviour when requesting a password reset from the HOME page
+    // with valid credentials
+    public function test_home_reset_valid_credentials(): void
+    {
+        // create fake user
+        $user = Account::factory()->create([
+            'accountNo' => 'AAAAAA2',
+            'password' => Hash::make(fake()->regexify('testPassword1')),
+        ]);
+
+        // request while acting as user, assert successful
+        $response = $this->actingAs($user)->post('/change-password', [
+            'accountNo' => 'AAAAAA2',
+            'currentPassword' => 'testPassword1',
+            'password' => 'testPassword2',
+            'password_confirmation' => 'testPassword2'
+        ])->assertStatus(200);
+    }
+
+
+
+    // Test for correct behaviour when requesting a password reset from the HOME page
+    // with no credentials
+    public function test_home_reset_no_credentials(): void
+    {
+        // create fake user
+        $user = Account::factory()->create([
+            'accountNo' => 'AAAAAA2',
+            'password' => Hash::make(fake()->regexify('testPassword1')),
+        ]);
+
+        // request while acting as user, assert 302 (failed validation)
+        $response = $this->actingAs($user)->post('/change-password', [
+            'accountNo' => '',
+            'currentPassword' => '',
+            'password' => '',
+            'password_confirmation' => ''
+        ])->assertStatus(302);
+    }
+
+
+
+    // Test for correct behaviour when requesting a password reset from the HOME page
+    // with no credentials
+    public function test_home_reset_passwords_dont_match(): void
+    {
+        // create fake user
+        $user = Account::factory()->create([
+            'accountNo' => 'AAAAAA2',
+            'password' => Hash::make(fake()->regexify('testPassword1')),
+        ]);
+
+        // request while acting as user, assert 302 (failed validation)
+        $response = $this->actingAs($user)->post('/change-password', [
+            'accountNo' => 'AAAAAA2',
+            'currentPassword' => 'testPassword1',
+            'password' => 'testPassword2',
+            'password_confirmation' => 'testPassword3'
+        ])->assertStatus(302);
+    }
+
+
+
+    // Test for correct behaviour when resetting your password from the home page,
+    // where the current password is incorrect
+    public function test_home_reset_current_password_wrong(): void
+    {
+        // create fake user
+        $user = Account::factory()->create([
+            'accountNo' => 'AAAAAA2',
+            'password' => Hash::make(fake()->regexify('testPassword1')),
+        ]);
+
+        // request while acting as user, assert 302 (failed validation)
+        $response = $this->actingAs($user)->post('/change-password', [
+            'accountNo' => 'AAAAAA2',
+            'currentPassword' => 'testPassword5',
+            'password' => 'testPassword2',
+            'password_confirmation' => 'testPassword2'
+        ])->assertStatus(302);
     }
 }
--- tests/Unit/PasswordResetControllerTest.php
+++ tests/Unit/PasswordResetControllerTest.php
@@ -39,128 +39,128 @@ class PasswordResetControllerTest extends TestCase
 
 
 
-    // Test the sending of a password reset email using valid credentials
-    public function test_sending_reset_email_valid_credentials(): void
-    {
-        $response = $this->post('/reset-password', [
-            'email' => 'AAAAAA1@curtin.edu.au',
-            'accountNo' => 'AAAAAA1'
-        ])->assertStatus(200);
+    // // Test the sending of a password reset email using valid credentials
+    // public function test_sending_reset_email_valid_credentials(): void
+    // {
+    //     $response = $this->post('/reset-password', [
+    //         'email' => 'AAAAAA1@curtin.edu.au',
+    //         'accountNo' => 'AAAAAA1'
+    //     ])->assertStatus(200);
 
-        $this->assertJson($response->content());
+    //     $this->assertJson($response->content());
 
-        $response->assertJson([
-            'status' => 'We have emailed your password reset link.',
-        ]);
-    }
-
-
-
-    // Test for correct behaviour when request a password reset email with no credentials provided
-    public function test_sending_reset_email_no_credentials(): void
-    {
-        $response = $this->post('/reset-password', [
-            'email' => 'AAAAAB1@curtin.edu.au',
-            'accountNo' => 'AAAAAB1'
-        ])->assertStatus(302);
-
-        // Except the validation exception from no email
-        $this->expectException(ValidationException::class);
-        $response->assertJson([
-            'message' => 'The email field must be a valid email address.',
-        ]);
-    }
+    //     $response->assertJson([
+    //         'status' => 'We have emailed your password reset link.',
+    //     ]);
+    // }
 
 
 
-    // Test for correct behaviour when request a password reset email with invalid credentials provided
-    public function test_sending_reset_email_bad_credentials(): void
-    {
-        $response = $this->post('/reset-password', [
-            'email' => 'AAAAAB1',
-            'accountNo' => 'AAAAAB1'
-        ])->assertStatus(302);
+    // // Test for correct behaviour when request a password reset email with no credentials provided
+    // public function test_sending_reset_email_no_credentials(): void
+    // {
+    //     $response = $this->post('/reset-password', [
+    //         'email' => 'AAAAAB1@curtin.edu.au',
+    //         'accountNo' => 'AAAAAB1'
+    //     ])->assertStatus(302);
 
-        // Except the validation exception from no email
-        $this->expectException(ValidationException::class);
-        $response->assertJson([
-            'message' => 'The email field must be a valid email address.',
-        ]);
-    }
+    //     // Except the validation exception from no email
+    //     $this->expectException(ValidationException::class);
+    //     $response->assertJson([
+    //         'message' => 'The email field must be a valid email address.',
+    //     ]);
+    // }
 
 
 
-    // Test for correct behaviour when requesting a password reset from the HOME page
-    // with valid credentials
-    public function test_home_reset_valid_credentials(): void
-    {
-        // create fake user
-        $user = Account::factory()->create([
-            'accountNo' => 'AAAAAA2',
-            'password' => Hash::make(fake()->regexify('testPassword1')),
-        ]);
+    // // Test for correct behaviour when request a password reset email with invalid credentials provided
+    // public function test_sending_reset_email_bad_credentials(): void
+    // {
+    //     $response = $this->post('/reset-password', [
+    //         'email' => 'AAAAAB1',
+    //         'accountNo' => 'AAAAAB1'
+    //     ])->assertStatus(302);
 
-        // request while acting as user, assert successful
-        $response = $this->actingAs($user)->post('/change-password', [
-            'accountNo' => 'AAAAAA2',
-            'currentPassword' => 'testPassword1',
-            'password' => 'testPassword2',
-            'password_confirmation' => 'testPassword2'
-        ])->assertStatus(200);
-    }
+    //     // Except the validation exception from no email
+    //     $this->expectException(ValidationException::class);
+    //     $response->assertJson([
+    //         'message' => 'The email field must be a valid email address.',
+    //     ]);
+    // }
 
-    // Test for correct behaviour when requesting a password reset from the HOME page
-    // with no credentials
-    public function test_home_reset_no_credentials(): void
-    {
-        // create fake user
-        $user = Account::factory()->create([
-            'accountNo' => 'AAAAAA2',
-            'password' => Hash::make(fake()->regexify('testPassword1')),
-        ]);
 
-        // request while acting as user, assert 302 (failed validation)
-        $response = $this->actingAs($user)->post('/change-password', [
-            'accountNo' => '',
-            'currentPassword' => '',
-            'password' => '',
-            'password_confirmation' => ''
-        ])->assertStatus(302);
-    }
 
-    // Test for correct behaviour when requesting a password reset from the HOME page
-    // with no credentials
-    public function test_home_reset_passwords_dont_match(): void
-    {
-        // create fake user
-        $user = Account::factory()->create([
-            'accountNo' => 'AAAAAA2',
-            'password' => Hash::make(fake()->regexify('testPassword1')),
-        ]);
+    // // Test for correct behaviour when requesting a password reset from the HOME page
+    // // with valid credentials
+    // public function test_home_reset_valid_credentials(): void
+    // {
+    //     // create fake user
+    //     $user = Account::factory()->create([
+    //         'accountNo' => 'AAAAAA2',
+    //         'password' => Hash::make(fake()->regexify('testPassword1')),
+    //     ]);
 
-        // request while acting as user, assert 302 (failed validation)
-        $response = $this->actingAs($user)->post('/change-password', [
-            'accountNo' => 'AAAAAA2',
-            'currentPassword' => 'testPassword1',
-            'password' => 'testPassword2',
-            'password_confirmation' => 'testPassword3'
-        ])->assertStatus(302);
-    }
+    //     // request while acting as user, assert successful
+    //     $response = $this->actingAs($user)->post('/change-password', [
+    //         'accountNo' => 'AAAAAA2',
+    //         'currentPassword' => 'testPassword1',
+    //         'password' => 'testPassword2',
+    //         'password_confirmation' => 'testPassword2'
+    //     ])->assertStatus(200);
+    // }
 
-    public function test_home_reset_current_password_wrong(): void
-    {
-        // create fake user
-        $user = Account::factory()->create([
-            'accountNo' => 'AAAAAA2',
-            'password' => Hash::make(fake()->regexify('testPassword1')),
-        ]);
+    // // Test for correct behaviour when requesting a password reset from the HOME page
+    // // with no credentials
+    // public function test_home_reset_no_credentials(): void
+    // {
+    //     // create fake user
+    //     $user = Account::factory()->create([
+    //         'accountNo' => 'AAAAAA2',
+    //         'password' => Hash::make(fake()->regexify('testPassword1')),
+    //     ]);
 
-        // request while acting as user, assert 302 (failed validation)
-        $response = $this->actingAs($user)->post('/change-password', [
-            'accountNo' => 'AAAAAA2',
-            'currentPassword' => 'testPassword5',
-            'password' => 'testPassword2',
-            'password_confirmation' => 'testPassword2'
-        ])->assertStatus(302);
-    }
+    //     // request while acting as user, assert 302 (failed validation)
+    //     $response = $this->actingAs($user)->post('/change-password', [
+    //         'accountNo' => '',
+    //         'currentPassword' => '',
+    //         'password' => '',
+    //         'password_confirmation' => ''
+    //     ])->assertStatus(302);
+    // }
+
+    // // Test for correct behaviour when requesting a password reset from the HOME page
+    // // with no credentials
+    // public function test_home_reset_passwords_dont_match(): void
+    // {
+    //     // create fake user
+    //     $user = Account::factory()->create([
+    //         'accountNo' => 'AAAAAA2',
+    //         'password' => Hash::make(fake()->regexify('testPassword1')),
+    //     ]);
+
+    //     // request while acting as user, assert 302 (failed validation)
+    //     $response = $this->actingAs($user)->post('/change-password', [
+    //         'accountNo' => 'AAAAAA2',
+    //         'currentPassword' => 'testPassword1',
+    //         'password' => 'testPassword2',
+    //         'password_confirmation' => 'testPassword3'
+    //     ])->assertStatus(302);
+    // }
+
+    // public function test_home_reset_current_password_wrong(): void
+    // {
+    //     // create fake user
+    //     $user = Account::factory()->create([
+    //         'accountNo' => 'AAAAAA2',
+    //         'password' => Hash::make(fake()->regexify('testPassword1')),
+    //     ]);
+
+    //     // request while acting as user, assert 302 (failed validation)
+    //     $response = $this->actingAs($user)->post('/change-password', [
+    //         'accountNo' => 'AAAAAA2',
+    //         'currentPassword' => 'testPassword5',
+    //         'password' => 'testPassword2',
+    //         'password_confirmation' => 'testPassword2'
+    //     ])->assertStatus(302);
+    // }
 }
