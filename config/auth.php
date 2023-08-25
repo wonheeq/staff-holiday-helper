1d1cd4fe11f0d5f68b22e3bfc3a3a8dd14a2a010
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-25 19:35:13 +0800

    Undid last commit

--- config/auth.php
+++ config/auth.php
@@ -96,12 +96,17 @@ return [
     */
 
     'passwords' => [
-
+        // 'users' => [
+        //     'provider' => 'users',
+        //     'table' => 'password_reset_tokens',
+        //     'expire' => 60,
+        //     'throttle' => 60,
+        // ],
         'accounts' => [
             'provider' => 'users',
             'table' => 'password_reset_tokens',
             'expire' => 60,
-            'throttle' => 300,
+            // 'throttle' => 60,
         ],
     ],
 
