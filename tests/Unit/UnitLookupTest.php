9c0eee04228b9ffa2dfd2460b7e37919f02de3c2
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-27 14:49:37 +0800

    Cleanup

--- tests/Unit/UnitLookupTest.php
+++ tests/Unit/UnitLookupTest.php
@@ -21,28 +21,6 @@ class UnitLookupTest extends TestCase
     protected function setup(): void
     {
         parent::setup();
-        // delete any leftover applications
-        $this->deleteNominations('000000g');
-        $this->deleteAllApplications();
-
-        // delete created roles
-        $this->deleteAccountRole('000000b', 1);
-        $this->deleteAccountRole('000000c', 2);
-        $this->deleteAccountRole('000000d', 3);
-        $this->deleteAccountRole('000000e', 4);
-        $this->deleteAccountRole('000000f', 4);
-
-        // delete created accounts
-        Account::where('accountNo', '000000b')->delete();
-        Account::where('accountNo', '000000c')->delete();
-        Account::where('accountNo', '000000d')->delete();
-        Account::where('accountNo', '000000e')->delete();
-        Account::where('accountNo', '000000f')->delete();
-        Account::where('accountNo', '000000g')->delete();
-
-        // delete test unit
-        Unit::where('unitId', 'AAAA0000')->delete();
-        Unit::where('unitId', 'BBBB0000')->delete();
 
         // create test accounts
         $this->createAccount("000000b"); // UnitCoord
