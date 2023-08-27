57f865077305860150c605a561bec2b60e3480b1
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-27 20:41:03 +0800

    Refactor/use-store-for-substitutions/34

---
+++ resources/js/stores/SubstitutionStore.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+import axios from "axios";
+
+export let useSubstitutionStore = defineStore('substitutions', {
+    state: () => ({
+        substitutions: [],
+    }),
+
+    actions: {
+        async fetchSubstitutions(accountNo) {
+            try {
+                const resp = await axios.get('/api/getSubstitutionsForUser/' + accountNo);
+                this.substitutions = resp.data;
+              }
+              catch (error) {
+                alert(error)
+                console.log(error)
+            }
+        }
+    },
+/*
+    getters: {
+        filteredMessages(viewing) {
+            return this.messages.filter(message => message.acknowledged === 0);
+        }
+    }
+*/
+});
