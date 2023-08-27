92f25b12a4a0ffbfc3a6065385da5c260dd018da
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-27 18:59:29 +0800

    Removed old logic

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -71,21 +71,6 @@ class ResetPassword extends Notification
         return $mailable->to($notifiable->getEmailForPasswordReset());
     }
 
-    /**
-     * Get the reset password notification mail message for the given URL.
-     *
-     * @param  string  $url
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    protected function buildMailMessage($url)
-    {
-        return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
-    }
 
     /**
      * Get the reset URL for the given notifiable.
