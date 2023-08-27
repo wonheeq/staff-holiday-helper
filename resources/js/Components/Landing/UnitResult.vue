ce75a9f3994b3ca1460e7d6ba55908c9aa2b2008
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-08-27 14:40:46 +0800

    Improved Frontend display

--- resources/js/Components/Landing/UnitResult.vue
+++ resources/js/Components/Landing/UnitResult.vue
@@ -62,30 +62,42 @@ function getList()
         <div class="mb-7 overflow-auto  1440:max-h-[37rem] 1080:max-h-[32rem] laptop:max-h-[23rem] 4k:max-h-[50rem]">
 
             <!-- Course Coordinator -->
-            <div v-show="hasCC">
+            <div>
                 <h2 class="mt-5 mb-1 font-bold 4k:text-xl">Course Coordinator:</h2>
-                <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.courseCoord[1] }}</h1>
-                <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.courseCoord[0] }}</h1>
+                <div v-show="hasCC">
+                    <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.courseCoord[1] }}</h1>
+                    <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.courseCoord[0] }}</h1>
+                </div>
+                <h1 v-show="!hasCC" class="mb-1 4k:text-xl indent-10">None Found</h1>
             </div>
+
 
             <!-- Major Coordinator -->
-            <div v-show="hasMC">
+            <div>
                 <h2 class="mt-3 mb-1 font-bold 4k:text-xl">Major Coordinator:</h2>
-                <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.majorCoord[1] }}</h1>
-                <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.majorCoord[0] }}</h1>
+                <div v-show="hasMC">
+                    <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.majorCoord[1] }}</h1>
+                    <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.majorCoord[0] }}</h1>
+                </div>
+                <h1 v-show="!hasMC" class="mb-1 4k:text-xl indent-10">None Found</h1>
             </div>
 
+
             <!-- Unit Coordinator -->
-            <div v-show="hasUC">
+            <div>
                 <h2 class="mt-3 mb-1 font-bold 4k:text-xl">Unit Coordinator:</h2>
-                <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.unitCoord[1] }}</h1>
-                <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.unitCoord[0] }}</h1>
+                <div v-show="hasUC">
+                    <h1 class="mb-1 4k:text-xl indent-10" >Name: {{ results.data.unitCoord[1] }}</h1>
+                    <h1 class="mb-1 4k:text-xl indent-10" >Email: {{ results.data.unitCoord[0] }}</h1>
+                </div>
+                <h1 v-show="!hasUC" class="mb-1 4k:text-xl indent-10">None Found</h1>
             </div>
 
             <!-- Lecturers -->
-            <div v-show="hasLec">
+            <div>
                 <h2 class="mt-3 mb-1 font-bold 4k:text-xl">Lecturers:</h2>
-                <div class="mb-7 ml-10 4k:text-xl whitespace-pre-line">{{  getList() }}</div>
+                <div v-show="hasLec" class="mb-7 ml-10 4k:text-xl whitespace-pre-line">{{  getList() }}</div>
+                <h1 v-show="!hasLec" class="mb-1 4k:text-xl indent-10">None Found</h1>
             </div>
 
         </div>
