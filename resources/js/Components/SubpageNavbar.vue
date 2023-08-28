a932336124eac462168d9af7b8d0a7200e094b99
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-08-28 20:42:42 +0800

    Fix inconsistent sizing in subpagenavbar mobile

--- resources/js/Components/SubpageNavbar.vue
+++ resources/js/Components/SubpageNavbar.vue
@@ -15,17 +15,23 @@ function isMobile() {
 }
 </script>
 <template>
-    <div class="flex space-x-4">
-        <button class="w-1/3 laptop:w-80 px-2 rounded-tl-md rounded-tr-md"
+    <div v-if="isMobile()"  class="flex space-x-4">
+        <button class="w-1/3 h-full laptop:w-80 px-2 rounded-tl-md rounded-tr-md text-xs"
             :class="{ 'bg-white': activeScreen===option.id, 'bg-gray-300': activeScreen!==option.id }"
             v-for="option in options"
             @click="activeScreen = option.id; emit('screen-changed', option.id)"
         >
-            <p v-if="isMobile()" class="text-xs">
-                {{ option.mobileTitle }}
-            </p>
-            <p v-else class="text-xs laptop:text-lg 1080:text-xl 1440:text-2xl text-center">
-                {{ option.mobileTitle }}
+        {{ option.mobileTitle }}
+        </button>
+    </div>
+    <div v-else class="flex space-x-4">
+        <button class="w-1/3 h-full laptop:w-80 px-2 rounded-tl-md rounded-tr-md"
+            :class="{ 'bg-white': activeScreen===option.id, 'bg-gray-300': activeScreen!==option.id }"
+            v-for="option in options"
+            @click="activeScreen = option.id; emit('screen-changed', option.id)"
+        >
+            <p class="text-xs laptop:text-lg 1080:text-xl 1440:text-2xl text-center">
+                {{ option.title }}
             </p>
         </button>
     </div>
