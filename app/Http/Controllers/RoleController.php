441ddea523a1b565b5d2c651709f5c5381eca0ed
Aden Moore <20562959@student.curtin.edu.au>
2023-08-30 14:52:06 +0800

    deleted duplicate method in RoleController

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -73,26 +73,4 @@ class RoleController extends Controller
             return response()->json($roles);  
         }  
     }
-
-    /*
-    Returns all Roles
-     */
-    public function getAllRoles(Request $request, String $accountNo)
-    {  
-        // Check if user exists for given accountNo
-        if (!Account::where('accountNo', $accountNo)->first()) {
-            // User does not exist, return exception
-            return response()->json(['error' => 'Account does not exist.'], 500);
-        }
-        else {
-            // Verify that the account is a system admin account
-            if (!Account::where('accountNo', $accountNo)->where('accountType', 'sysadmin')->first()) {
-                // User is not a system admin, deny access to full table
-                return response()->json(['error' => 'User not authorized for request.'], 500);
-            }
-
-            $roles = Role::get();
-            return response()->json($roles);  
-        }  
-    }
 }
