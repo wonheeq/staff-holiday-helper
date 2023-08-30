f40dc4f99463918f4d5a54442ccfbe61eaa1064f
Benjamin <20456890@student.curtin.edu.au>
2023-08-30 17:07:09 +0800

    Refactor accept & reject application handle

--- resources/js/Components/Manager/ApplicationInfo.vue
+++ resources/js/Components/Manager/ApplicationInfo.vue
@@ -1,70 +1,33 @@
 <script setup>
+import ReviewApplication from '@/Components/ReviewApplication.vue';
 import Swal from 'sweetalert2';
 import axios from 'axios';
-import { storeToRefs } from 'pinia';
-import { useUserStore } from '@/stores/UserStore';
-let userStore = useUserStore();
-const { userId } = storeToRefs(userStore);
+import { usePage } from '@inertiajs/vue3'
+const page = usePage();
+import {computed, reactive, ref} from 'vue';
+const user = computed(() => page.props.auth.user);
+
 let props = defineProps({ source: Object });
-
-
-function handleAccept() {
-    let data = {
-        'accountNo': props.source.accountNo,
-        'applicationNo': props.source.applicationNo,
-        'processedBy': null
-    };
-    axios.post('/api/acceptApplication', data)
-        .then(res => {
-            if (res.status == 500) {
-                Swal.fire({
-                    icon: "error",
-                    title: 'Failed to accept application, please try again.',
-                    text: res.message
-                });
-            }
-            else {
-                // Set application status to Y
-                props.source.status = 'Y';
-                props.source.updated_at = new Date();
-            }
-        }).catch(err => {
-        console.log(err);
-        Swal.fire({
-            icon: "error",
-            title: 'Failed to accept applications, please try again.',
-        });
-    });
+let reviewAppModalData = reactive([]);
+let showReviewAppModal = ref(false);
+    
+let fetchApplicationForReview = async() => {
+    try {
+        const resp = await axios.get('/api/getApplicationForReview/' + user.value.accountNo + "/" + props.source.applicationNo);
+        reviewAppModalData = resp.data;
+    } catch (error) {
+        alert("Failed to load data: Please try again");
+        console.log(error);
+    }
+}; 
+function handleCloseReviewApp() {
+    reviewAppModalData = [];
+    showReviewAppModal.value = false;
 }
 
-function handleReject() {
-    let data = {
-        'accountNo': props.source.accountNo,
-        'applicationNo': props.source.applicationNo,
-        'rejectReason': props.source.rejectReason,
-        'processedBy' : null
-    };
-    axios.post('/api/rejectApplication', data)
-        .then(res => {
-            if (res.status == 500) {
-                Swal.fire({
-                    icon: "error",
-                    title: 'Failed to accept application, please try again.',
-                    text: res.message
-                });
-            }
-            else {
-                // Set application status to 'N'
-                props.source.status = 'N';
-                props.source.updated_at = new Date();
-            }
-        }).catch(err => {
-        console.log(err);
-        Swal.fire({
-            icon: "error",
-            title: 'Failed to reject applications, please try again.',
-        });
-    });
+async function handleReviewApplication() {
+    await fetchApplicationForReview();
+    showReviewAppModal.value = true;
 }
 </script>
 <template>
@@ -80,26 +43,18 @@ function handleReject() {
             <div>
                 <p class="pt-2 text-2xl">Substitutes (The following staffs have agreed to substitute for the following roles):</p>
                 <div v-if="!source.isSelfNominatedAll" v-for="nomination in source.nominations">
-                    <p class="text-2xl" v-if="nomination.nomineeNo != userId">
+                    <p class="text-2xl" v-if="nomination.nomineeNo != user.accountNo">
                         → {{ nomination.name }} ({{ nomination.nomineeNo }}) - {{ nomination.task }} - {{ nomination.nomineeNo }}@curtin.edu.au
                     </p>
                 </div>
             </div>
-        </div>  
-        <div class="flex flex-col w-1/6 bg-gray-200 text-3xl ml-2 p-2 justify-center items-center">
-            <button class="flex flex-col items-center p-11"
-                @click="handleAccept()"
-            >
-            <img src="/images/accept.svg" style="width: 100px; height: 100px;"/>
-            <p class="text-sm 1440:text-lg">Accept</p>
-            </button>
-        </div>
+        </div>          
         <div class="flex flex-col w-1/6 bg-gray-200 text-3xl ml-2 p-2 justify-center items-center">
             <button class="flex flex-col items-center"
-                @click="handleReject()"
+                @click="handleReviewApplication()"
             >
-                <img src="/images/reject.svg" style="width: 100px; height: 100px;"/>
-                <p class="text-sm 1440:text-lg">Reject</p>
+                <img src="/images/review-app.svg" style="width: 100px; height: 100px;"/>
+                <p class="text-sm 1440:text-lg">Review</p>
             </button>
         </div>
     </div>
@@ -113,7 +68,7 @@ function handleReject() {
             <div>
                 <p class="pt-2 text-2xl">Substitutes (The following staffs have agreed to substitute for the following roles):</p>
                 <div v-if="!source.isSelfNominatedAll" v-for="nomination in source.nominations">
-                    <p class="text-2xl" v-if="nomination.nomineeNo != userId">
+                    <p class="text-2xl" v-if="nomination.nomineeNo != user.accountNo">
                         → {{ nomination.name }} ({{ nomination.nomineeNo }}) - {{ nomination.task }} - {{ nomination.nomineeNo }}@curtin.edu.au
                     </p>
                 </div>
@@ -132,6 +87,13 @@ function handleReject() {
             </p>
         </div>
     </div>
+    <Teleport to="body">
+        <ReviewApplication
+            v-show="showReviewAppModal"
+            :data="reviewAppModalData"
+            @close="handleCloseReviewApp()"
+        />
+    </Teleport>
 </template>
 
 <style>
