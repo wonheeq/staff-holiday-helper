56dbb51adb40532f5f412277b938ff0c379709c6
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-09-08 18:36:40 +0800

    Replace isMobile() for SubpageNavbar

--- resources/js/Components/SubpageNavbar.vue
+++ resources/js/Components/SubpageNavbar.vue
@@ -1,21 +1,16 @@
 <script setup>
-//import NavLink from '@/Components/NavLink.vue';
+import { storeToRefs } from 'pinia';
+import { useScreenSizeStore } from '@/stores/ScreenSizeStore';
+const screenSizeStore = useScreenSizeStore();
+const { isMobile } = storeToRefs(screenSizeStore);
 const props = defineProps({
     options: Object,
     activeScreen: String,
 });
 const emit = defineEmits(['screen-changed']);
-function isMobile() {
-    if( screen.availWidth <= 760 ) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 </script>
 <template>
-    <div v-if="isMobile()"  class="flex space-x-4">
+    <div v-if="isMobile"  class="flex space-x-4">
         <button class="w-1/3 h-8 laptop:w-80 px-2 rounded-tl-md rounded-tr-md text-xs"
             :class="{ 'bg-white': activeScreen===option.id, 'bg-gray-300': activeScreen!==option.id }"
             v-for="option in options"
