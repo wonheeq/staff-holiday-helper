34c33a8ff056b2194de354a6f6441e08412aa105
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-09-10 16:49:50 +0800

    Fixed Enter submission on home page

--- resources/js/Components/SettingsModal.vue
+++ resources/js/Components/SettingsModal.vue
@@ -152,7 +152,7 @@ async function handleReset() {
                                 class="w-full 4k:h-16 4k:text-2xl"
                                 :type="fieldType.current.type"
                             >
-                            <button @click.prevent="switchVis(fieldType.current)" tabindex="-1" class="fixed right-5">
+                            <button @click.prevent="switchVis(fieldType.current)" type="button" tabindex="-1" class="fixed right-5">
                                 <img :src="fieldType.current.image"
                                     class="h-full w-full">
                             </button>
@@ -165,7 +165,7 @@ async function handleReset() {
                                 class="w-full 4k:h-16 4k:text-2xl"
                                 :type="fieldType.password.type"
                             >
-                            <button @click.prevent="switchVis(fieldType.password)" tabindex="-1" class="fixed right-5">
+                            <button @click.prevent="switchVis(fieldType.password)" type="button" tabindex="-1" class="fixed right-5">
                                 <img :src="fieldType.password.image"
                                     class="h-full w-full">
                             </button>
@@ -178,7 +178,7 @@ async function handleReset() {
                                 class="w-full 4k:h-16 4k:text-2xl"
                                 :type="fieldType.confirm.type"
                             >
-                            <button @click.prevent="switchVis(fieldType.confirm)" tabindex="-1" class="fixed right-5">
+                            <button @click.prevent="switchVis(fieldType.confirm)" type="button" tabindex="-1" class="fixed right-5">
                                 <img :src="fieldType.confirm.image"
                                     class="h-full w-full">
                             </button>
