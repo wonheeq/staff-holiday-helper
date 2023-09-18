bcfb7fb22abb20f0d64002c14a13d9478d47cb89
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-09-18 18:27:25 +0800

    Base Notification and Emails

---
+++ app/Notifications/NewMessages.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Mail\Mailable;
+use App\Mail\MJML;
+
+class NewMessages extends Notification
+{
+    use Queueable;
+
+    public $messages;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($messages)
+    {
+        $this->messages = $messages;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): Mailable
+    {
+        $dynamicData = [];
+        $mailable = new MJML("temp", "temp", $dynamicData);
+        return $mailable;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
