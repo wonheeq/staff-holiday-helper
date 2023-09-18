8363d043a0d6b09aa2006254365eb3c45d15982a
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-09-18 16:14:09 +0800

    Frontend support for System Notification messages

--- resources/js/Components/Message.vue
+++ resources/js/Components/Message.vue
@@ -25,30 +25,36 @@ function handleReviewApplication() {
 
 <template>
     <div class="flex flex-row justify-between bg-gray-200 p-2">
-        <div class="flex flex-col w-[75%] laptop:w-full">
-            <div class="flex flex-col laptop:flex-row items-center">
-                <p class="text-sm 1080:text-lg 1440:text-xl 4k:text-2xl font-bold">
-                    {{ props.source.subject }}
+        <div v-if="props.source.subject !== 'System Notification'" class="flex flex-col w-[75%] laptop:w-full">
+            <div class="flex laptop:flex-row items-center">
+                <p class="text-sm 1080:text-lg 1440:text-xl 4k:text-2xl">
+                    <b>{{ props.source.subject }}</b> by {{ props.source.senderName }}
                 </p>
-                <div class="flex">
-                    <p class="text-sm 1080:text-lg 1440:text-xl 4k:text-2xl ml-2">
-                        by {{ props.source.senderName }}
-                    </p>
-                    <input
-                        type="image"
-                        v-if="props.source.senderNo != null"
-                        class="ml-1.5 email"
-                        src="/images/mail.svg"
-                        title="Copy Email Address to Clipboard"
-                        @click="copyEmail(props.source.senderNo)"
-                        v-title
-                        />
-                </div>
+                <input
+                    type="image"
+                    v-if="props.source.senderNo != null"
+                    class="ml-1.5 email"
+                    src="/images/mail.svg"
+                    title="Copy Email Address to Clipboard"
+                    @click="copyEmail(props.source.senderNo)"
+                    v-title
+                />
             </div>
             <p class="text-xs 1080:text-base 1440:text-lg 4k:text-xl"
                 v-for="content in JSON.parse(props.source.content)"
             >
                 {{ content }}
+            </p>
+            <p class="text-xs 1080:text-sm 1440:text-base 4k:text-xl">Message created at {{ new Date(props.source.created_at).toLocaleString() }}</p>
+        </div>
+        <div v-else class="flex flex-col w-[75%] laptop:w-full">
+            <div class="flex laptop:flex-row items-center">
+                <p class="text-sm 1080:text-lg 1440:text-xl 4k:text-2xl">
+                    <b>{{ props.source.subject }}</b>
+                </p>
+            </div>
+            <p class="text-xs 1080:text-base 1440:text-lg 4k:text-xl">
+                {{ JSON.parse(props.source.content) }}
             </p>
             <p class="text-xs 1080:text-sm 1440:text-base 4k:text-xl">Message created at {{ new Date(props.source.created_at).toLocaleString() }}</p>
         </div>
