a6fb61fc62436ae0625555d6f1ae93874fa26718
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-09-27 19:55:38 +0800

    Refactor SubpageNavbar for darkmode

--- resources/js/Components/SubpageNavbar.vue
+++ resources/js/Components/SubpageNavbar.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { storeToRefs } from 'pinia';
 import { useScreenSizeStore } from '@/stores/ScreenSizeStore';
+import { useDark } from "@vueuse/core";
+const isDark = useDark();
 const screenSizeStore = useScreenSizeStore();
 const { isMobile } = storeToRefs(screenSizeStore);
 const props = defineProps({
@@ -12,7 +14,12 @@ const emit = defineEmits(['screen-changed']);
 <template>
     <div v-if="isMobile"  class="flex space-x-4">
         <button class="w-1/3 h-8 laptop:w-80 px-2 rounded-tl-md rounded-tr-md text-xs"
-            :class="{ 'bg-white': activeScreen===option.id, 'bg-gray-300': activeScreen!==option.id }"
+            :class="{
+                'bg-white': activeScreen===option.id && !isDark,
+                'bg-gray-800': activeScreen===option.id && isDark,
+                'bg-gray-300': activeScreen!==option.id && !isDark,
+                'bg-gray-500': activeScreen!==option.id && isDark,
+            }"
             v-for="option in options"
             @click="activeScreen = option.id; emit('screen-changed', option.id)"
         >
@@ -21,7 +28,12 @@ const emit = defineEmits(['screen-changed']);
     </div>
     <div v-else class="flex space-x-4">
         <button class="w-1/3 h-full laptop:w-80 px-2 rounded-tl-md rounded-tr-md"
-            :class="{ 'bg-white': activeScreen===option.id, 'bg-gray-300': activeScreen!==option.id }"
+            :class="{
+                'bg-white': activeScreen===option.id && !isDark,
+                'bg-gray-800': activeScreen===option.id && isDark,
+                'bg-gray-300': activeScreen!==option.id && !isDark,
+                'bg-gray-500': activeScreen!==option.id && isDark,
+            }"
             v-for="option in options"
             @click="activeScreen = option.id; emit('screen-changed', option.id)"
         >
