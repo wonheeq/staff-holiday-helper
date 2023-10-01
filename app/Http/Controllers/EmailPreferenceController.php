b3a31c25da56b6658f87806f71da165633e142b8
Ellis <ellis.jansonferrall@student.curtin.edu.au>
2023-10-01 14:09:57 +0800

    Controller and Upate Function

---
+++ app/Http/Controllers/EmailPreferenceController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Account;
+use App\Models\EmailPreference;
+
+class EmailPreferenceController extends Controller
+{
+
+    // Set the interval for a users archive email
+    public function setPreference(Request $request)
+    {
+        $request->validate([
+            'accountNo' => ['required'],
+            'hours' => ['required']
+        ]);
+        $accountNo = $request->accountNo;
+        $frequency = $request->hours;
+        if(!EmailPreference::where('accountNo', $accountNo)->first())
+        {
+            return response()->json(['error' => 'Account does not exist.'], 500);
+        }
+        else
+        {
+            $user = EmailPreference::where('accountNo', $accountNo)->first();
+            $user->hours = $frequency;
+            $user->save();
+            return response(200);
+        }
+    }
+}
