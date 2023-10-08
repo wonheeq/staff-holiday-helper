719b36a79df7b062c4367ac9f14950d178ffdfbe
Benjamin <20456890@student.curtin.edu.au>
2023-10-09 01:36:34 +0800

    Added mobile dark mode for reviewapplication info mobile

--- resources/js/Components/ReviewApplication.vue
+++ resources/js/Components/ReviewApplication.vue
@@ -7,8 +7,12 @@ import VueScrollingTable from "vue-scrolling-table";
 import "/node_modules/vue-scrolling-table/dist/style.css";
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import { storeToRefs } from 'pinia';
 import { usePage } from '@inertiajs/vue3'
 import { useDark } from "@vueuse/core";
+import { useScreenSizeStore } from '@/stores/ScreenSizeStore';
+const screenSizeStore = useScreenSizeStore();
+const { isMobile } = storeToRefs(screenSizeStore);
 const isDark = useDark();
 const page = usePage();
 const user = computed(() => page.props.auth.user);
@@ -65,6 +69,7 @@ function handleApproveApp() {
                 if(props.data.message != null){
                     props.data.message.acknowledged = 1;
                     props.data.message.updated_at = new Date();
+                    props.data.status = 'Y';
                 }
                 
                 Swal.fire({
@@ -104,6 +109,7 @@ function handleRejectApp() {
                 if(props.data.message != null){
                     props.data.message.acknowledged = 1;
                     props.data.message.updated_at = new Date();
+                    props.data.status = 'Y';
                 }
                 Swal.fire({
                     icon: "success",
@@ -133,77 +139,156 @@ const buttonClass = "p-4 w-1/3 rounded-md text-white text-2xl font-bold";
 </script>
 
 <template>
-<Modal>
-    <div class="w-3/5 1080:w-1/2 1440:w-2/6 h-[32rem] 1080:h-[48rem] rounded-md p-4" :class="isDark?'bg-gray-800':'bg-white'" v-if="props.data">
-        <div class="flex h-[10%] items-center justify-between">
-            <p class="text-2xl 1080:text-3xl 4k:text-5xl font-bold" :class="isDark?'text-white':''">
-                Reviewing Application by {{ props.data.applicantName }}
-            </p>
-            <button @click="handleClose()">
-                    <img src="/images/close.svg"
-                    class="close-button h-full"
+    <Modal>
+        <div class="w-4/5 h=4/5 fixed rounded-md p-3" :class="isDark?'bg-gray-800':'bg-white'" v-if="props.data && isMobile">
+            <div class="flex h-[10%] items-center justify-between" :class="isDark?'text-white':''">
+                <p class="text-s font-bold">
+                    Reviewing Application by {{ props.data.applicantName }}
+                </p>
+                <button @click="handleClose()">
+                    <img src="/images/close.svg" 
+                    style="width: 30px; height: 30px;" 
                     :class="isDark?'darkModeImage':''"
-                />
-            </button>
-        </div>
-        <div class="flex h-[6%] items-center justify-between">
-            <p class="text-xl 1080:text-2xl 4k:text-3xl" :class="isDark?'text-white':''">
-                Duration: {{ props.data.duration }}
-            </p>
-        </div>
-        <div class="h-[45%] py-4">
-            <VueScrollingTable
-                :deadAreaColor="deadAreaColor"
-                :scrollHorizontal="false"
-            >
-                <template #tbody>
-                    <div class="mb-2"
-                        v-for="nomination in props.data.nominations" :key="nomination.id">
-                        <p v-if="nomination.nomineeNo !== props.data.applicantNo"
-                            class="text-lg 1080:text-xl 4k:text-2xl"
-                            :class="isDark?'text-white':''"
-                        >
-                            {{ nomination.nomineeName }}
-                        </p>
-                        <p v-if="nomination.nomineeNo == props.data.applicantNo"
-                            class="text-lg 1080:text-xl 4k:text-2xl"
-                            :class="isDark?'text-white':''"
-                        >
-                            Self-Nomination
-                        </p>
-                        <div>
-                            <p v-for="role in nomination.roles" :class="isDark?'text-white':''">
-                                →{{ role }}
+                    />
+                </button>
+            </div>
+            <div class="flex h-[10%] items-center justify-between pt-2">
+                <p class="text-sm" :class="isDark?'text-white':''">
+                    Duration: {{ props.data.duration }}
+                </p>
+            </div>
+            <div class="h-[35%] py-2">
+                <VueScrollingTable
+                    :deadAreaColor="deadAreaColor"
+                    :scrollHorizontal="false"
+                >
+                    <template #tbody>
+                        <div class="mb-2"
+                            v-for="nomination in props.data.nominations" :key="nomination.id">
+                            <p v-if="nomination.nomineeNo !== props.data.applicantNo"
+                                class="text-sm"
+                                :class="isDark?'text-white':''"
+                            >
+                                {{ nomination.nomineeName }}
                             </p>
+                            <p v-if="nomination.nomineeNo == props.data.applicantNo"
+                                class="text-s"
+                                :class="isDark?'text-white':''"
+                            >
+                                Self-Nomination
+                            </p>
+                            <div>
+                                <p v-for="role in nomination.roles"
+                                    class="text-xs"
+                                    :class="isDark?'text-white':''"
+                                >
+                                    →{{ role }}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </template>
-            </VueScrollingTable>
+                    </template>
+                </VueScrollingTable>
+            </div>
+            <div class="h-[29%] py-4">
+                <p class="text-sm h-[20%]pb-8" :class="isDark?'text-white':''"> 
+                    Select Reject Reason or Enter Custom Message:
+                </p>
+                <input type="text" v-model="rejectReason" class="h-[40%] w-full border-gray-300 border-2 rounded-md p-2 text-xs" :class="isDark?'text-white bg-gray-800':''"/>
+                <vSelect :options="options" :clearable="false" :class="isDark ? 'dropdown-dark':''"
+                    style="width: 100%; height: 2rem; background-color: white;
+                    font-size: 10px; 
+                    border: solid; border-color: #6b7280; border-width: 1px;
+                    --vs-border-style: none; --vs-search-input-placeholder-color: #6b7280"                                 
+                    v-model="multiSelectNominee"
+                    @option:selected="(selectedOption) => handleSelection(selectedOption)"
+                    :isDisabled="selfNominateAll"
+                />
+            </div>
+            <div class="flex justify-between pt-5">
+                <button :class="buttonClass" class="bg-green-500 p-0 text-base" @click="handleApproveApp()">
+                    Approve
+                </button>
+                <button :class="rejectReason=='' ? disabledClass : buttonClass + ' bg-red-500'" class="text-base p-0"
+                    :disabled="!buttonActive"
+                    @click="handleRejectApp()"
+                >
+                    Reject
+                </button>
+            </div>
         </div>
-        <div class="h-[29%] py-4 space-y-2">
-            <p class="h-[20%] text-lg 1080:text-xl 4k:text-2xl" :class="isDark?'text-white':''">
-                Select Reject Reason or Enter Custom Message:
-            </p>
-            <input type="text" v-model="rejectReason" class="h-[40%] w-full border-gray-300 border-2 rounded-md p-2" :class="isDark?'text-white bg-gray-800':''"/>
-            <vSelect :options="options" :clearable="false" :class="isDark ? 'dropdown-dark':''"
-                style="width: 100%; height: 2rem; background-color: inherit;"                 
-                v-model="rejectReason"
-                @option:selected="(selectedOption) => handleSelection(selectedOption)"
-            />
+        <div class="w-3/5 1080:w-1/2 1440:w-2/6 h-[32rem] 1080:h-[48rem] rounded-md p-4" :class="isDark?'bg-gray-800':'bg-white'" v-if="props.data && !isMobile">
+            <div class="flex h-[10%] items-center justify-between" :class="isDark?'text-white':''">
+                <p class="text-lg laptop:text-base 1080:text-3xl 1440:text-2xl 4k:text-4xl font-bold" :class="isDark?'text-white':''">
+                    Reviewing Application by {{ props.data.applicantName }}
+                </p>
+                <button @click="handleClose()">
+                        <img src="/images/close.svg"
+                        class="close-button h-full"
+                        :class="isDark?'darkModeImage':''"
+                    />
+                </button>
+            </div>
+            <div class="flex h-[6%] items-center justify-between">
+                <p class="text-m laptop:text-base 1080:text-3xl 1440:text-2xl 4k:text-4xl" :class="isDark?'text-white':''">
+                    Duration: {{ props.data.duration }}
+                </p>
+            </div>
+            <div class="h-[45%] py-4">
+                <VueScrollingTable
+                    :deadAreaColor="deadAreaColor"
+                    :scrollHorizontal="false"
+                >
+                    <template #tbody>
+                        <div class="mb-2"
+                            v-for="nomination in props.data.nominations" :key="nomination.id">
+                            <p v-if="nomination.nomineeNo !== props.data.applicantNo"
+                                class="text-lg 1080:text-xl 4k:text-2xl"
+                                :class="isDark?'text-white':''"
+                            >
+                                {{ nomination.nomineeName }}
+                            </p>
+                            <p v-if="nomination.nomineeNo == props.data.applicantNo"
+                                class="text-lg 1080:text-xl 4k:text-2xl"
+                                :class="isDark?'text-white':''"
+                            >
+                                Self-Nomination
+                            </p>
+                            <div>
+                                <p :class="isDark?'text-white':''" v-for="role in nomination.roles">
+                                    →{{ role }}
+                                </p>
+                            </div>
+                        </div>
+                    </template>
+                </VueScrollingTable>
+            </div>
+            <div class="h-[29%] py-4">
+                <p class="h-[20%] text-lg 1080:text-xl 4k:text-2xl pb-8" :class="isDark?'text-white':''">
+                    Select Reject Reason or Enter Custom Message:
+                </p>
+                <input type="text" v-model="rejectReason" class="h-[40%] w-full border-gray-300 border-2 rounded-md p-2" :class="isDark?'text-white bg-gray-800':''"/>
+                <vSelect :options="options" :clearable="false" :class="isDark ? 'dropdown-dark':''"
+                    style="width: 100%; height: 2rem; background-color: white; 
+                    border: solid; border-color: #6b7280; border-width: 1px;
+                    --vs-border-style: none; --vs-search-input-placeholder-color: #6b7280"                                 
+                    v-model="multiSelectNominee"
+                    @option:selected="(selectedOption) => handleSelection(selectedOption)"
+                    :isDisabled="selfNominateAll"
+                />
+            </div>
+            <div class="h-[10%] flex justify-between">
+                <button :class="buttonClass" class="bg-green-500 p-0" @click="handleApproveApp()">
+                    Approve
+                </button>
+                <button :class="rejectReason=='' ? disabledClass : buttonClass + ' bg-red-500'" class="p-0"
+                    :disabled="!buttonActive"
+                    @click="handleRejectApp()"
+                >
+                    Reject
+                </button>
+            </div>
         </div>
-        <div class="h-[10%] flex justify-between">
-            <button :class="buttonClass" class="bg-green-500" @click="handleApproveApp()">
-                Approve
-            </button>
-            <button :class="rejectReason=='' ? disabledClass : buttonClass + ' bg-red-500'"
-                :disabled="!buttonActive"
-                @click="handleRejectApp()"
-            >
-                Reject
-            </button>
-        </div>
-    </div>
-</Modal>
+    </Modal>
 </template>
 
 <style>
