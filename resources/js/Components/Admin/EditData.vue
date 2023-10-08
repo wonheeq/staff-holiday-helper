d2f12962d74b9c369dbefd5c0a6acf528fa78878
Aden Moore <20562959@student.curtin.edu.au>
2023-10-09 03:03:08 +0800

    Edit popup scaling

--- resources/js/Components/Admin/EditData.vue
+++ resources/js/Components/Admin/EditData.vue
@@ -52,7 +52,7 @@
 <template>
     <Modal>
         <div class="h-screen flex items-center w-screen bg-transparent">
-            <div class="w-3/6 flex flex-col p-4 mx-auto h-4/7 rounded-tl-md overflow-auto" :class="subpageClass">
+            <div class="w-3/6 4k:w-[70rem] flex flex-col p-4 mx-auto h-4/7 4k:h-[48rem] rounded-tl-md overflow-auto" :class="subpageClass">
                 <div class="h-[10%] flex justify-between 4k:ml-6">
                     <slot />
                     <p class="text-xl mt-1 font-bold 4k:text-3xl 4k:mt-6">             
@@ -64,13 +64,13 @@
                         <img src="/images/close.svg" class="h-full w-full"/>
                     </button>
                 </div>
-                <p class="4k:text-2xl">             
+                <p class="4k:text-2xl 4k:ml-6 4k:mt-3">             
                     Re-fill the values you wish to change.
                 </p>
-                <p  v-if="table == 'Staff Accounts'" class="4k:text-2xl text-red-700">             
+                <p  v-if="table == 'Staff Accounts'" class="4k:text-2xl text-red-700 4k:ml-6">             
                     Note: Changing your own account details will require you to log back in.
                 </p>
-                <div class="mt-3 4k:ml-6">
+                <div class="mt-3 ">
                     <div v-for="(, index) in entry" :key="index">
                         <div class="flex justify-between mx-4 4k:mx-6">
                             <span class="mt-4 4k:mt-10 4k:text-2xl">{{ index }}: </span>
@@ -82,7 +82,7 @@
                     </div>
                 </div>
                 <div class="flex mt-5 justify-center">
-                    <button class="bg-[#e3e3e3] px-6 py-2 ml-4 text-center w-28 text-lg 4k:text-3xl"
+                    <button class="bg-[#e3e3e3] px-6 py-2 ml-4 text-center w-28 text-lg 4k:text-3xl 4k:mt-2 4k:w-36"
                         @click="updateTuple()">
                         Update 
                     </button>
