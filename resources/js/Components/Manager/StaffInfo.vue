23f68b5daae2b123ce9d25c0b030a7fcc53ef331
Benjamin <20456890@student.curtin.edu.au>
2023-10-09 01:40:56 +0800

    Applied mobile dark mode for staff info page

--- resources/js/Components/Manager/StaffInfo.vue
+++ resources/js/Components/Manager/StaffInfo.vue
@@ -1,11 +1,37 @@
 <script setup>
+import { storeToRefs } from 'pinia';
 import { useDark } from "@vueuse/core";
+import { useScreenSizeStore } from '@/stores/ScreenSizeStore';
+const screenSizeStore = useScreenSizeStore();
+const { isMobile } = storeToRefs(screenSizeStore);
 const isDark = useDark();
 let props = defineProps({ source: Object });
 let emit = defineEmits(['editRoles']);
 </script>
 <template>
-    <div class="grid grid-cols-6 mr-4 mx-6" style="grid-auto-columns: min-content;" :class="isDark?'bg-gray-800':'bg-white'"> 
+    <div v-if="isMobile" class="text-sm" :class="isDark?'bg-gray-800':'bg-white'" >
+        <div>
+            <p class="staff-text"><strong>Name: </strong>{{ source.fName }} {{ source.lName }}</p>
+        </div>
+        <div class="shrink-1">
+            <p class="staff-text"><strong>ID: </strong>{{ source.accountNo }}</p>
+        </div>
+        <div>
+            <p class="staff-text"><strong>Email Address: </strong>{{ source.accountNo }}@curtin.edu.au</p>
+        </div>
+        <div>
+            <p class="staff-text"><strong>On Leave: </strong>{{ source.onLeave }}</p>
+        </div>
+        <div>
+            <p class="staff-text"><strong>Pending Application: </strong>{{ source.pending }}</p>
+        </div>
+        <div class="flex justify-center items-center">
+            <button @click="emit('editRoles', source.accountNo)" class="roles_buttonMobile">
+                <span class="button-text">View/Edit Roles</span>
+            </button>
+        </div>
+    </div>
+    <div v-else class="grid grid-cols-6 mr-4 mx-6" style="grid-auto-columns: min-content;" :class="isDark?'bg-gray-800':'bg-white'"> 
         <div>
             <p class="staff-text">{{ source.fName }} {{ source.lName }} </p>
         </div>
@@ -29,10 +55,9 @@ let emit = defineEmits(['editRoles']);
             </div>
         </div>
     </div>
-</template>
 <style>
 .roles_button {
-  display: flex;
+    display: flex;
   justify-content: center;
   align-items: center; 
   background-color: #ccc; 
@@ -45,6 +70,21 @@ let emit = defineEmits(['editRoles']);
 .roles_button:active{
     background-color: #999;
 }
+.roles_buttonMobile {
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    background-color: #ccc; 
+    border: none;
+    padding: 2px 29px;
+    width: 100%;
+    border-radius: 4px; 
+    cursor: pointer;
+    transition: background-color 0.2s;
+  }
+  .roles_buttonMobile:active{
+      background-color: #999;
+  }
 .button-text {
     font-weight: bold;
     color: black; 
