2cb2c689d0462211df314a5a4eaa48861047806c
Won Hee Qin <wonhee.qin@student.curtin.edu.au>
2023-10-09 15:29:02 +0800

    AddCSVData mobile & darkmode

--- resources/js/Components/Admin/AddCSVData.vue
+++ resources/js/Components/Admin/AddCSVData.vue
@@ -4,6 +4,12 @@
     import axios from "axios";
     import Swal from 'sweetalert2';
 
+    import { useDark } from "@vueuse/core";
+    import { storeToRefs } from 'pinia';
+    import { useScreenSizeStore } from '@/stores/ScreenSizeStore';
+    const screenSizeStore = useScreenSizeStore();
+    const { isMobile } = storeToRefs(screenSizeStore);
+    const isDark = useDark();
     let emit = defineEmits(['close']);
     var uploadedFile = null;
     const fileInputKey = ref(0);
@@ -147,23 +153,23 @@
             console.log(msg.errorMsg);
         }
     }
-
-    const subpageClass = "rounded-bl-md rounded-br-md laptop:rounded-tr-md bg-white";
 </script>
 
 
 <template>
     <Modal>
         <div class="h-screen flex items-center w-screen bg-transparent">
-            <div class="w-3/6 flex flex-col p-4 mx-auto h-4/6 rounded-tl-md overflow-auto" :class="subpageClass">
+            <div class="mx-4 laptop:w-3/6 flex flex-col p-4 laptop:mx-auto h-4/6 rounded-tl-md overflow-auto rounded-bl-md rounded-br-md laptop:rounded-tr-md"
+                :class="isDark?'bg-gray-800':'bg-white'"
+            >
                 <div class="h-[10%] flex justify-between 4k:ml-6">
                     <slot />
-                    <p class="text-xl font-bold 4k:text-3xl 4k:mt-6">             
+                    <p class="text-xl mr-4 font-bold 4k:text-3xl 4k:mt-6">             
                         Add data to {{ curTable }} table with .csv file:
                     </p>
                     <!--Add full ui then worry about implementation-->
                     <button class="h-full" @click="$emit('close')">
-                        <img src="/images/close.svg" class="h-2/3 w-2/3"/>
+                        <img src="/images/close.svg" class="h-2/3 w-2/3" :class="isDark?'darkModeImage':''"/>
                     </button>
                 </div> 
                 <div class="mt-6 4k:ml-6">
@@ -173,22 +179,27 @@
                     <button class="mt-6 underline 4k:text-2xl" @click="csvRequested()">    
                         Download CSV Template {{ csvFileName }}    
                     </button>
-                    <p class="mt-20 mb-8 4k:text-2xl">    
+                    <p class="mt-6 laptop:mt-20 mb-8 4k:text-2xl">    
                         Note: Only .csv files accepted, if the file was converted into another type while being filled in it must first be converted back to .csv type before uploading.
                     </p>
-                    <div class="flex">
-                        <input type="file" class="bg-[#e3e3e3] pl-2 py-2 ml-4 text-center text-lg 4k:text-3xl 4k:pl-5 4k:py-5"
-                               @change="csvSubmitted"
-                               accept=".csv"
-                               :key="fileInputKey">
-                        
-                        <button class="bg-[#e3e3e3] px-6 py-2 ml-4 text-center text-lg 4k:text-3xl"
+                    <div class="flex flex-col laptop:flex-row">
+                        <input type="file" class="laptop:pl-2 py-2 laptop:ml-4 text-center text-lg 4k:text-3xl 4k:pl-5 4k:py-5"
+                            :class="isDark?'':'bg-[#e3e3e3]'"
+                            @change="csvSubmitted"
+                            accept=".csv"
+                            :key="fileInputKey">
+                    
+                        <button class="px-6 py-2 laptop:ml-4 text-center text-lg 4k:text-3xl"
+                            :class="isDark?'bg-gray-600':'bg-[#e3e3e3]'"   
                             @click="csvPosted()">
                             Upload .CSV
                         </button>
                     </div>
-                    <h4 class="mx-4 mt-6 text-sm text-red-700 4k:text-2xl 4k:mt-9" v-show="msg.warning">
-                        <span v-html="msg.errorMsg"></span>
+                    <h4 class="mx-2 laptop:mx-4 mt-6 text-sm 4k:text-2xl 4k:mt-9"
+                        v-show="msg.warning"
+                        :class="isDark?'text-red-500':'text-red-700'"    
+                    >
+                        <span v-html="msg.errorMsg" ></span>
                     </h4>
                 </div>
             </div>
